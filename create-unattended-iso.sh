abdcaea71a49c100f93d383a0f0026d0061f1b18
Jonathan Brain <3941269+BrainforgeUK@users.noreply.github.com>
2018-07-28 13:11:10 +0100

    Customised for Brainforge.UK

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # file names & paths
-tmp="$HOME"  # destination folder to store the final iso file
-hostname="ubuntu"
+pwd=`pwd`
+tmp=$pwd/tmp  # destination folder to store the final iso file
+mkdir $tmp >/dev/null 2>&1
 currentuser="$( whoami)"
 
 # define spinner function for slow tasks
@@ -32,17 +33,6 @@ download()
         sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
     echo -ne "\b\b\b\b"
     echo " DONE"
-}
-
-# define function to check if program is installed
-# courtesy of https://gist.github.com/JamieMason/4761049
-function program_is_installed {
-    # set to 1 initially
-    local return_=1
-    # set to 0 if not found
-    type $1 >/dev/null 2>&1 || { local return_=0; }
-    # return value
-    echo $return_
 }
 
 # print a pretty header
@@ -76,8 +66,6 @@ trus=$(fgrep Trusty $tmphtml | head -1 | awk '{print $3}')
 xenn=$(fgrep Xenial $tmphtml | head -1 | awk '{print $3}')
 bion=$(fgrep Bionic $tmphtml | head -1 | awk '{print $3}')
 
-
-
 # ask whether to include vmware tools or not
 while true; do
     echo " which ubuntu edition would you like to remaster:"
@@ -109,31 +97,6 @@ while true; do
     esac
 done
 
-if [ -f /etc/timezone ]; then
-  timezone=`cat /etc/timezone`
-elif [ -h /etc/localtime ]; then
-  timezone=`readlink /etc/localtime | sed "s/\/usr\/share\/zoneinfo\///"`
-else
-  checksum=`md5sum /etc/localtime | cut -d' ' -f1`
-  timezone=`find /usr/share/zoneinfo/ -type f -exec md5sum {} \; | grep "^$checksum" | sed "s/.*\/usr\/share\/zoneinfo\///" | head -n 1`
-fi
-
-# ask the user questions about his/her preferences
-read -ep " please enter your preferred timezone: " -i "${timezone}" timezone
-read -ep " please enter your preferred username: " -i "netson" username
-read -sp " please enter your preferred password: " password
-printf "\n"
-read -sp " confirm your preferred password: " password2
-printf "\n"
-read -ep " Make ISO bootable via USB: " -i "yes" bootable
-
-# check if the passwords match to prevent headaches
-if [[ "$password" != "$password2" ]]; then
-    echo " your passwords do not match; please restart the script and try again"
-    echo
-    exit
-fi
-
 # download the ubunto iso. If it already exists, do not delete in the end.
 cd $tmp
 if [[ ! -f $tmp/$download_file ]]; then
@@ -141,42 +104,13 @@ if [[ ! -f $tmp/$download_file ]]; then
     download "$download_location$download_file"
 fi
 if [[ ! -f $tmp/$download_file ]]; then
-	echo "Error: Failed to download ISO: $download_location$download_file"
-	echo "This file may have moved or may no longer exist."
-	echo
-	echo "You can download it manually and move it to $tmp/$download_file"
-	echo "Then run this script again."
-	exit 1
+        echo "Error: Failed to download ISO: $download_location$download_file"
+        echo "This file may have moved or may no longer exist."
+        echo
+        echo "You can download it manually and move it to $tmp/$download_file"
+        echo "Then run this script again."
+        exit 1
 fi
-
-# download netson seed file
-seed_file="netson.seed"
-if [[ ! -f $tmp/$seed_file ]]; then
-    echo -n " downloading $seed_file: "
-    download "https://raw.githubusercontent.com/netson/ubuntu-unattended/master/$seed_file"
-fi
-
-# install required packages
-echo " installing required packages"
-if [ $(program_is_installed "mkpasswd") -eq 0 ] || [ $(program_is_installed "mkisofs") -eq 0 ]; then
-    (apt-get -y update > /dev/null 2>&1) &
-    spinner $!
-    (apt-get -y install whois genisoimage > /dev/null 2>&1) &
-    spinner $!
-fi
-if [[ $bootable == "yes" ]] || [[ $bootable == "y" ]]; then
-    if [ $(program_is_installed "isohybrid") -eq 0 ]; then
-      #16.04
-      if [[ $ub1604 == "yes" || $(lsb_release -cs) == "artful" ]]; then
-        (apt-get -y install syslinux syslinux-utils > /dev/null 2>&1) &
-        spinner $!
-      else
-        (apt-get -y install syslinux > /dev/null 2>&1) &
-        spinner $!
-      fi
-    fi
-fi
-
 
 # create working folders
 echo " remastering your iso file"
@@ -204,51 +138,25 @@ echo en > $tmp/iso_new/isolinux/lang
 #taken from https://github.com/fries/prepare-ubuntu-unattended-install-iso/blob/master/make.sh
 sed -i -r 's/timeout\s+[0-9]+/timeout 1/g' $tmp/iso_new/isolinux/isolinux.cfg
 
-
-# set late command
-
-   late_command="chroot /target curl -L -o /home/$username/start.sh https://raw.githubusercontent.com/netson/ubuntu-unattended/master/start.sh ;\
-     chroot /target chmod +x /home/$username/start.sh ;"
-
-
-
 # copy the netson seed file to the iso
-cp -rT $tmp/$seed_file $tmp/iso_new/preseed/$seed_file
-
-# include firstrun script
-echo "
-# setup firstrun script
-d-i preseed/late_command                                    string      $late_command" >> $tmp/iso_new/preseed/$seed_file
-
-# generate the password hash
-pwhash=$(echo $password | mkpasswd -s -m sha-512)
-
-# update the seed file to reflect the users' choices
-# the normal separator for sed is /, but both the password and the timezone may contain it
-# so instead, I am using @
-sed -i "s@{{username}}@$username@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{pwhash}}@$pwhash@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{hostname}}@$hostname@g" $tmp/iso_new/preseed/$seed_file
-sed -i "s@{{timezone}}@$timezone@g" $tmp/iso_new/preseed/$seed_file
+if ! cp -rT $pwd/brainforge.seed $tmp/iso_new/preseed/brainforge.seed
+then
+  exit 1
+fi
 
 # calculate checksum for seed file
-seed_checksum=$(md5sum $tmp/iso_new/preseed/$seed_file)
+seed_checksum=$(md5sum $tmp/iso_new/preseed/brainforge.seed)
 
 # add the autoinstall option to the menu
 sed -i "/label install/ilabel autoinstall\n\
   menu label ^Autoinstall NETSON Ubuntu Server\n\
   kernel /install/vmlinuz\n\
-  append file=/cdrom/preseed/ubuntu-server.seed initrd=/install/initrd.gz auto=true priority=high preseed/file=/cdrom/preseed/netson.seed preseed/file/checksum=$seed_checksum --" $tmp/iso_new/isolinux/txt.cfg
+  append file=/cdrom/preseed/ubuntu-server.seed initrd=/install/initrd.gz auto=true priority=high preseed/file=/cdrom/preseed/brainforge.seed preseed/file/checksum=$seed_checksum --" $tmp/iso_new/isolinux/txt.cfg
 
 echo " creating the remastered iso"
 cd $tmp/iso_new
 (mkisofs -D -r -V "NETSON_UBUNTU" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $tmp/$new_iso_name . > /dev/null 2>&1) &
 spinner $!
-
-# make iso bootable (for dd'ing to  USB stick)
-if [[ $bootable == "yes" ]] || [[ $bootable == "y" ]]; then
-    isohybrid $tmp/$new_iso_name
-fi
 
 # cleanup
 umount $tmp/iso_org
@@ -261,20 +169,10 @@ rm -rf $tmphtml
 echo " -----"
 echo " finished remastering your ubuntu iso file"
 echo " the new file is located at: $tmp/$new_iso_name"
-echo " your username is: $username"
-echo " your password is: $password"
-echo " your hostname is: $hostname"
-echo " your timezone is: $timezone"
 echo
 
 # unset vars
-unset username
-unset password
-unset hostname
-unset timezone
-unset pwhash
 unset download_file
 unset download_location
 unset new_iso_name
 unset tmp
-unset seed_file
